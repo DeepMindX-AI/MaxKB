29333a867f7dbe3c97bc269610e739969db0eb1f
JackeyLove1 <1572727410@qq.com>
2024-04-21 10:41:55 +0800

    feature: support wenxin model

--- apps/setting/models_provider/constants/model_provider_constants.py
+++ apps/setting/models_provider/constants/model_provider_constants.py
@@ -9,11 +9,11 @@
 from enum import Enum
 
 from setting.models_provider.impl.openai_model_provider.openai_model_provider import OpenAIModelProvider
-# from setting.models_provider.impl.wenxin_model_provider.wenxin_model_provider import WenxinModelProvider
+from setting.models_provider.impl.wenxin_model_provider.wenxin_model_provider import WenxinModelProvider
 # from setting.models_provider.impl.azure_model_provider.azure_model_provider import AzureModelProvider
 # from setting.models_provider.impl.ollama_model_provider.ollama_model_provider import OllamaModelProvider
 class ModelProvideConstants(Enum):
     model_openai_provider = OpenAIModelProvider()
+    model_wenxin_provider = WenxinModelProvider()
     # model_azure_provider = AzureModelProvider()
-    # model_wenxin_provider = WenxinModelProvider()
     # model_ollama_provider = OllamaModelProvider()
